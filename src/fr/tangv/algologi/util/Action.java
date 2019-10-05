0dac259dae26f3fd88d3b866e7431066e6dfd08a
Tangvfr <tangv.minecraft@gmail.com>
2019-10-05 14:07:19 +0200

    .

--- src/fr/tangv/algologi/util/Action.java
+++ src/fr/tangv/algologi/util/Action.java
@@ -1,5 +1,8 @@
 package fr.tangv.algologi.util;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -60,6 +63,38 @@ public class Action {
 	
 	public void setNext(Map<String, Action> next) {
 		this.next = next;
+	}
+	
+	public BufferedImage render() {
+		BufferedImage img = new BufferedImage(50, 30, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics g = img.getGraphics();
+		switch (type) {
+			case action:
+				break;
+			case condiction:
+				break;
+			case end:
+				
+				break;
+			case excuteFunction:
+				break;
+			case point:
+				break;
+			case start:
+				g.setColor(Color.BLACK);
+				g.fillRect(25, 15, 2, 15);
+				g.setColor(Color.green);
+				g.fillOval(5, 5, 40, 20);
+				g.setColor(Color.BLACK);
+				g.drawOval(5, 5, 40, 20);
+				break;
+			case gotoPoint:
+				
+				break;
+			default:
+				break;
+		}
+		return img;
 	}
 	
 	public Action clone(Action prev) {
--- src/fr/tangv/algologi/util/Algo.java
+++ src/fr/tangv/algologi/util/Algo.java
@@ -1,5 +1,9 @@
 package fr.tangv.algologi.util;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,8 +19,36 @@ public class Algo {
 		return methodes;
 	}
 	
-	public void generatedImage() {
+	public BufferedImage renderAction(Action action) {
+		Action ac = action;
+		while (ac.getPrev() != null) {
+			ac = ac.getPrev();
+		}
+		return action.render();
+	}
+	
+	public Image generatedImage() {
 		
+		//color
+		Color backGroundColor = Color.gray;
+		//image
+		BufferedImage img = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = img.getGraphics();
+		//cls
+		g.setColor(backGroundColor);
+		g.fillRect(0, 0, img.getWidth(), img.getHeight());
+		//generated
+		
+		for (String key : methodes.keySet()) {
+			Methode methode = methodes.get(key);
+			if (methode.isMain()) {
+				g.drawImage(renderAction(methode.getAction()), 0, 0, null);
+				break;
+			}
+		}
+		
+		//return
+		return img;
 	}
 	
 }
